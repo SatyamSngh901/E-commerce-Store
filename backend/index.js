d4e228d2b6062a3f942efa4bdd253cf270c2fe1d
Satyam Singh <satyamsngh901@gmail.com>
2026-01-28 20:44:24 +0530

    Configure backend for Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -36,4 +36,8 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => console.log(`Server running on port: ${port}`));
+}
+
+export default app;
