1848b3c7ba0af5b1192646a8547ae6913d1f2d3a
Satyam Singh <satyamsngh901@gmail.com>
2026-01-29 09:31:42 +0530

    Final fix for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+// proxy: {
+//   "/api/": "https://e-commerce-store-nrie.vercel.app",
+//   "/uploads/": "https://e-commerce-store-nrie.vercel.app",
+// },
+
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-proxy: {
-  "/api/": "https://e-commerce-store-nrie.vercel.app",
-  "/uploads/": "https://e-commerce-store-nrie.vercel.app",
-},
-
+    proxy: {
+      // This is ONLY for local development (npm run dev)
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
+    },
   },
 });
